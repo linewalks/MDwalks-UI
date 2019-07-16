e5dc1154f7b0840735336e8e483694cafe82d2f2
lyohan <jik0090@naver.com>
2019-07-16 10:22:54 +0900

    add summary card

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import BarGauge from './components/charts/BarGauge';
 import RadiusGauge from './components/charts/RadiusGauge';
+import SummaryCard from './components/charts/SummaryCard';
 
-export { BarGauge, RadiusGauge } 
+export { BarGauge, RadiusGauge, SummaryCard } 
