f340350fa8bb117d105dfd64e3740427d0e6f2fa
lyohan <jik0090@naver.com>
2019-07-30 17:40:35 +0900

    update testcase expect value

--- src/components/__tests__/BarGauge.test.js
+++ src/components/__tests__/BarGauge.test.js
@@ -5,12 +5,12 @@ import BarGauge from '@Charts/BarGauge';
 describe('BarGauge Component', () => {
   it('Score Props has vaild number 0', () => {
     const wrapper = shallow(<BarGauge score={0} />)
-    expect(wrapper.html().includes('0%')).toEqual(true);
+    expect(wrapper.html().includes('width:0%')).toEqual(true);
   })
 
   it('Score Props has vaild number 100', () => {
     const wrapper = shallow(<BarGauge score={100} />)
-    expect(wrapper.html().includes('100%')).toEqual(true);
+    expect(wrapper.html().includes('width:100%')).toEqual(true);
   })
 
   it('Score Props excluded Range renders error messege', () => {
