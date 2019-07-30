cd7057cb0523ff393bfd56e37fb2cd77b5fb6e94
lyohan <jik0090@naver.com>
2019-07-30 17:52:59 +0900

    add angle test case

--- src/components/__tests__/RadiusGauge.test.js
+++ src/components/__tests__/RadiusGauge.test.js
@@ -10,11 +10,27 @@ describe('RadiusGauge', () => {
 
   it('Score Props has vaild number 1', () => {
     const wrapper = shallow(<RadiusGauge width={300} height={200} score={1} />)
+    console.log('@',wrapper.html())
     expect(wrapper.html().includes('<text>1</text>')).toEqual(true);
   })
 
   it('Score Props excluded Range renders error messege', () => {
     const wrapper = shallow(<RadiusGauge width={300} height={200} score={10} />)
     expect(wrapper.html()).toEqual("<div>Invalid Score</div>")
+  })
+
+  it('Score Props Match Angle 0', () => {
+    const wrapper = shallow(<RadiusGauge width={300} height={200} score={0} />)
+    expect(wrapper.html().includes('rotate(0, 75, 3.1)')).toEqual(true)
+  })
+
+  it('Score Props Match Angle 90', () => {
+    const wrapper = shallow(<RadiusGauge width={300} height={200} score={0.5} />)
+    expect(wrapper.html().includes('rotate(90, 75, 3.1)')).toEqual(true)
+  })
+
+  it('Score Props Match Angle 180', () => {
+    const wrapper = shallow(<RadiusGauge width={300} height={200} score={1} />)
+    expect(wrapper.html().includes('rotate(180, 75, 3.1)')).toEqual(true)
   })
 })
