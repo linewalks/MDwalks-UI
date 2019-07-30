4da46584145c98c246d562dedc9a317a93feb1b7
lyohan <jik0090@naver.com>
2019-07-30 17:03:38 +0900

    add bargauge test

---
+++ src/components/__tests__/BarGauge.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import BarGauge from '@Charts/BarGauge';
+
+describe('BarGauge Component', () => {
+  it('Score Props has vaild number 0', () => {
+    const wrapper = shallow(<BarGauge score={0} />)
+    expect(wrapper.html().includes('0%')).toEqual(true);
+  })
+
+  it('Score Props has vaild number 100', () => {
+    const wrapper = shallow(<BarGauge score={100} />)
+    expect(wrapper.html().includes('100%')).toEqual(true);
+  })
+
+  it('Score Props excluded Range renders error messege', () => {
+    const wrapper = shallow(<BarGauge score={200} />)
+    expect(wrapper.html()).toEqual("<div>Invalid Score</div>")
+  })
+})
