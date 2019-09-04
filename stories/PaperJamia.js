ca2f293d5db241dd23915c8363b539c6b9df0288
hcinyoung <hcinyoung@system@linewalks.com>
2019-09-04 15:58:34 +0900

    Update layout

--- stories/PaperJamia.js
+++ stories/PaperJamia.js
@@ -3,40 +3,78 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { withKnobs } from "@storybook/addon-knobs";
 import Table from "@Table/Table";
-import SelectedCard from '@Card/SelectedCard';
+import SelectedCard from "@Card/SelectedCard";
 import SankeyChart from "@Charts/SankeyChart";
 import sankeyData from "@Data/dataForSankey";
 
+class Fig1 extends Component {
+  state = {
+    selectedNodes: ["emergency", "VSURG", "CSRU", "ECHO", "died"],
+    dataForTable: null
+  };
+
+  onChange(selectedNodes) {
+    this.setState({
+      selectedNodes,
+      selectPage: 1,
+    })
+
+    session.setCookie('selectedNodes', selectedNodes)
+
+    axios.all([
+      apiClient.pathway_events
+        .get({events: JSON.stringify(this.state.selectedNodes)}),
+      apiClient.pathway_patient_list
+        .get({events: JSON.stringify(this.state.selectedNodes), page: 1, length: 10}),
+    ]).then(axios.spread((events, patientList) => {
+      this.setState({
+        summaryData: utils.convertPathwayEvents(events.data),
+        dataForTable: utils.appendfolderIcon(utils.convertRowData(patientList.data)),
+      })
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        {/* /home/hcinyoung/dev/MDwalks-EXI-Cardio/frontend/src/pages/Home.js @193 */}
+        {/* /home/hcinyoung/dev/MDwalks-EXI-Cardio/frontend/src/pages/Home.js @205 */}
+        <SankeyChart
+          data={sankeyData}
+          selectedNodes={["emergency", "VSURG", "CSRU", "ECHO", "died"]}
+          onChange={this.onChange.bind(this)}
+          onNodeClick={action("Node has been clicked")}
+        />
+        <SelectedCard
+          selectedElement={["emergency", "VSURG", "CSRU", "ECHO", "died"]}
+        />
+        <Table
+          data={{
+            headers: ["a", "b", "c"],
+            rowData: [
+              {
+                a: 1,
+                b: 2,
+                c: 3
+              },
+              {
+                a: 4,
+                b: 5,
+                c: 6
+              },
+              {
+                a: 7,
+                b: 8,
+                c: 9
+              }
+            ]
+          }}
+        />
+      </div>
+    );
+  }
+}
+
 storiesOf("JAMIA", module)
   .addDecorator(withKnobs)
-  .add("fig1", () => (
-    <div>
-      <SankeyChart
-        data={sankeyData}
-        onNodeClick={action("Node has been clicked")}
-      />
-      <SelectedCard selectedElement={['a', 'b', 'c']} />
-      <Table
-        data={{
-          headers: ["a", "b", "c"],
-          rowData: [
-            {
-              a: 1,
-              b: 2,
-              c: 3
-            },
-            {
-              a: 4,
-              b: 5,
-              c: 6
-            },
-            {
-              a: 7,
-              b: 8,
-              c: 9
-            }
-          ]
-        }}
-      />
-    </div>
-  ));
+  .add("fig1", () => Fig1());
