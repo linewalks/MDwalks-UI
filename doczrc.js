7b2fbbdf9071afdd5e2ace61f68dc4111282de56
toyjhlee <toyjhlee@gmail.com>
2019-09-04 15:48:06 +0900

    Update readme

--- doczrc.js
+++ doczrc.js
@@ -2,8 +2,12 @@ export default {
   title: 'MDwalks-UI',
   native: true,
   typescript: false,
+  port: '9004',
   files: '**/*.{mdx}',
   ignore: ['CHANGELOG.md'],
+  htmlContext: {
+    favicon: '/favicon.ico'
+  },
   modifyBundlerConfig: (config) => {
     return config
   }
