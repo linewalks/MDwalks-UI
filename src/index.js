b89cfcfd79cbdf13ad85fbc27a2091cb7815b3a7
toyjhlee <toyjhlee@gmail.com>
2019-09-10 17:00:44 +0900

    Create Descriptions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import SankeyChart from './components/charts/SankeyChart';
 import SelectedCard from './components/card/SelectedCard';
 import SummaryCard from './components/card/SummaryCard';
 import Table from './components/table/Table';
+import Descriptions from './components/table/Descriptions';
 import LineChart from './components/charts/LineChart';
 import Timeline from './components/charts/Timeline'
 import LineMergeTimeline from './components/charts/LineMergeTimeline';
@@ -19,6 +20,7 @@ export {
   SelectedCard, 
   SummaryCard, 
   Table, 
+  Descriptions,
   LineChart,
   Timeline, 
   LineMergeTimeline,
