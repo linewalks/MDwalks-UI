a38a1ab8cf0339eaaa6c1413983f5cb063242157
toyjhlee <toyjhlee@gmail.com>
2019-11-27 12:25:13 +0900

    feat(checklist): change icon of checked

--- src/components/list/CheckList.js
+++ src/components/list/CheckList.js
@@ -1,34 +1,47 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import _ from 'lodash'
 import { font, variables } from '@src/index'
 import PropTypes from 'prop-types'
-import IcnAddSm from '@Components/list/icn-add-sm.svg'
+import IcnChecked from '@Components/list/check-box-checked-default.svg'
+import IcnUnchecked from '@Components/list/check-box-unchecked-default.svg'
+
+const CssEnable = css`
+  label {
+    cursor: pointer;
+  }
+  img:hover:not(:disabled) {
+    box-shadow: 0 2px 6px 0 rgba(0, 45, 79, 0.16);
+  }
+  &:hover {
+    background-color: ${variables.color.$secondary_blue};
+  }
+`
+
+const CssDiable = css`
+  img {
+    opacity: 0.4;
+  }
+`
 
 const Item = styled(font.TextTag).attrs(() => ({
   size: 16,
   opacity: 8,
 }))`
   label {
-    cursor: pointer;
     display: block;
     padding: 12px 24px;
     display: flex;
     align-items: center;
     img {
-      margin-left: auto;
-      visibility: hidden;
-    }
-  }
-  &:hover {
-    background-color: ${variables.color.$secondary_blue};
-    label img {
-      visibility: visible;
+      margin-right: 12px;
     }
   }
   input {
     display: none;
   }
+
+  ${(props) => (props.disabled ? CssDiable : CssEnable)}
 `
 
 class CheckList extends React.Component {
@@ -110,11 +123,12 @@ class CheckList extends React.Component {
 
             if (!checkVisible && checked) return null;
             return (
-              <Item size="16" opacity="6" as="div" key={`checkItem${id}`}>
+              <Item size="16" opacity="6" as="div" key={`checkItem${id}`} disabled={disabled}>
                 <label>
+                  <img src={checked ? IcnChecked : IcnUnchecked} width="24px" height="24px" alt="" />
                   <font.TextOverflow>{name}</font.TextOverflow>
                   <input type="checkbox" disabled={disabled} checked={checked} onChange={() => this.onChangeTrigger(id)} />
-                  <img src={IcnAddSm} width="24px" height="24px" alt="" />
+                  {/* <img src={IcnAddSm} width="24px" height="24px" alt="" /> */}
                 </label>
               </Item>
             )
