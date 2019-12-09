7420fd3ad8b6f271773c57d1185c68bec7d417b5
toyjhlee <toyjhlee@gmail.com>
2019-12-09 14:17:59 +0900

    improvement(Toast): apply eslint
    closes: #311

--- src/components/__tests__/ToastList.test.js
+++ src/components/__tests__/ToastList.test.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
-import ToastList  from '@Components/toast/ToastList';
-import Toast  from '@Components/toast/Toast';
-import ToastCtr  from '@Components/toast/ToastCtr';
+import ToastList from '@Components/toast/ToastList';
+import Toast from '@Components/toast/Toast';
+import ToastCtr from '@Components/toast/ToastCtr';
 
 describe('ToastList 추가 삭제', () => {
-  let wrapper, instance, initKey = 0
-
+  let wrapper; let instance
   beforeEach(() => {
-    wrapper = shallow(<ToastList></ToastList>)
+    wrapper = shallow(<ToastList />)
     instance = wrapper.instance()
   })
 
   it('추가', () => {
     expect(wrapper.find(Toast)).toHaveLength(0)
-    instance.addNotification({msg: 'aaa'})
+    instance.addNotification({ msg: 'aaa' })
     expect(wrapper.find(Toast)).toHaveLength(1)
-    instance.addNotification({msg: 'aaa'})
+    instance.addNotification({ msg: 'aaa' })
     expect(wrapper.find(Toast)).toHaveLength(2)
   })
 
   it('삭제', () => {
-    instance.addNotification({msg: 'aaa'})
+    instance.addNotification({ msg: 'aaa' })
     expect(wrapper.find(Toast)).toHaveLength(1)
     instance.removeNotification(wrapper.state('list')[0].id)
     expect(wrapper.find(Toast)).toHaveLength(0)
@@ -30,19 +29,19 @@ describe('ToastList 추가 삭제', () => {
 })
 
 describe('append', () => {
-  const document = global.document
+  const { document } = global
   const div = document.createElement('div');
   div.id = 'app-root'
   document.body.appendChild(div);
 
   ToastCtr.add([
-    {msg: 'Profile updated successfully!'},
+    { msg: 'Profile updated successfully!' },
   ])
 
   expect(document.getElementsByTagName('section')).toHaveLength(1)
 
   ToastCtr.add([
-    {type: 'error', msg: 'Email is already taken.'},
+    { type: 'error', msg: 'Email is already taken.' },
   ])
   expect(document.getElementsByTagName('section')).toHaveLength(2)
 })
