412eae28cadbc9672b7fe8f61e8011d843036098
toyjhlee <toyjhlee@gmail.com>
2020-01-08 16:48:26 +0900

    build v0.10.7

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -44371,8 +44371,10 @@ var ButtonLink = function ButtonLink(props) {
       children = props.children,
       size = props.size,
       style = props.style,
-      onClick = props.onClick;
+      onClick = props.onClick,
+      id = props.id;
   return React.createElement(ButtonLinkTag, {
+    id: id,
     as: propsAs,
     size: size,
     style: style,
@@ -44383,20 +44385,24 @@ ButtonLink.defaultProps = {
   as: 'a',
   size: 'md',
   styled: {},
-  onClick: function onClick() {}
+  onClick: function onClick() {},
+  id: undefined
 };
 ButtonLink.propTypes = {
   as: propTypes.string,
   size: propTypes.string,
   styled: propTypes.shape({}),
-  onClick: propTypes.func
+  onClick: propTypes.func,
+  id: propTypes.string
 };
 var ButtonTextLink = function ButtonTextLink(props) {
   var propsAs = props.as,
       children = props.children,
       style = props.style,
-      onClick = props.onClick;
+      onClick = props.onClick,
+      id = props.id;
   return React.createElement(ButtonTextLinkTag, {
+    id: id,
     as: propsAs,
     style: style,
     onClick: onClick
@@ -44405,12 +44411,14 @@ var ButtonTextLink = function ButtonTextLink(props) {
 ButtonTextLink.defaultProps = {
   as: 'a',
   styled: {},
-  onClick: function onClick() {}
+  onClick: function onClick() {},
+  id: undefined
 };
 ButtonTextLink.propTypes = {
   as: propTypes.string,
   styled: propTypes.shape({}),
-  onClick: propTypes.func
+  onClick: propTypes.func,
+  id: propTypes.string
 };
 
 var Button = function Button(props) {
@@ -44421,7 +44429,8 @@ var Button = function Button(props) {
       size = props.size,
       variant = props.variant,
       style = props.style,
-      onClick = props.onClick;
+      onClick = props.onClick,
+      id = props.id;
   var showLoading = isLoading;
 
   if (lodash.isString(isLoading)) {
@@ -44429,6 +44438,7 @@ var Button = function Button(props) {
   }
 
   return React.createElement(ButtonTag, {
+    id: id,
     as: propsAs,
     disabled: disabled || showLoading,
     size: size,
@@ -44445,7 +44455,8 @@ Button.defaultProps = {
   size: 'md',
   variant: 'basic_line',
   styled: {},
-  onClick: function onClick() {}
+  onClick: function onClick() {},
+  id: undefined
 };
 Button.propTypes = {
   isLoading: propTypes.oneOfType([propTypes.string, propTypes.bool]),
@@ -44454,7 +44465,8 @@ Button.propTypes = {
   size: propTypes.string,
   variant: propTypes.string,
   styled: propTypes.shape({}),
-  onClick: propTypes.func
+  onClick: propTypes.func,
+  id: propTypes.string
 };
 
 var IcnMessageError = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyOCIgaGVpZ2h0PSIyOCIgdmlld0JveD0iMCAwIDI4IDI4Ij4KICAgIDxkZWZzPgogICAgICAgIDxsaW5lYXJHcmFkaWVudCBpZD0iYSIgeDE9IjUwJSIgeDI9IjUwJSIgeTE9IjEyLjE3OCUiIHkyPSIxMDAlIj4KICAgICAgICAgICAgPHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iI0ZGM0MzQyIvPgogICAgICAgICAgICA8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNGRjkyODgiLz4KICAgICAgICA8L2xpbmVhckdyYWRpZW50PgogICAgPC9kZWZzPgogICAgPGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj4KICAgICAgICA8Y2lyY2xlIGN4PSIxNCIgY3k9IjE0IiByPSIxNCIgZmlsbD0idXJsKCNhKSIvPgogICAgICAgIDxwYXRoIGZpbGw9IiNGRkYiIGQ9Ik0xNCAxOWExIDEgMCAxIDEgMCAyIDEgMSAwIDAgMSAwLTJ6bS44ODMtMTFhMSAxIDAgMCAxIC45OTQgMS4xMWwtLjc3OCA3YTEgMSAwIDAgMS0uOTk0Ljg5aC0uMjFhMSAxIDAgMCAxLS45OTQtLjg5bC0uNzc4LTdBMSAxIDAgMCAxIDEzLjExNyA4aDEuNzY2eiIvPgogICAgPC9nPgo8L3N2Zz4=';
