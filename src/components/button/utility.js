f357ab429d1353050b277726dc4dc59858815f55
toyjhlee <toyjhlee@gmail.com>
2020-01-09 17:44:59 +0900

    improvement(utility): apply eslint

--- src/components/button/utility.js
+++ src/components/button/utility.js
@@ -1,9 +1,9 @@
 
 /* Suma el porcentaje indicado a un color (RR, GG o BB) hexadecimal para aclararlo */
-export const addLight = function(color, amount){
-  let cc = parseInt(color,16) + amount;
+export const addLight = (color, amount) => {
+  const cc = parseInt(color, 16) + amount;
   let c = (cc > 255) ? 255 : (cc);
-  c = (c.toString(16).length > 1 ) ? c.toString(16) : `0${c.toString(16)}`;
+  c = (c.toString(16).length > 1) ? c.toString(16) : `0${c.toString(16)}`;
   return c;
 }
 
@@ -13,37 +13,36 @@ export const addLight = function(color, amount){
 } */
 
 /* Aclara un color hexadecimal de 6 caracteres #RRGGBB segun el porcentaje indicado */
-export const lighten = (color, amount)=> {
-  color = (color.indexOf("#")>=0) ? color.substring(1,color.length) : color;
-  amount = parseInt((255*amount)/100);
-  return color = `#${addLight(color.substring(0,2), amount)}${addLight(color.substring(2,4), amount)}${addLight(color.substring(4,6), amount)}`;
+export const lighten = (colorOrigin, amountOrigin) => {
+  const color = (colorOrigin.indexOf('#') >= 0) ? colorOrigin.substring(1, colorOrigin.length) : colorOrigin;
+  const amount = parseInt((255 * amountOrigin) / 100, 10)
+  return `#${addLight(color.substring(0, 2), amount)}${addLight(color.substring(2, 4), amount)}${addLight(color.substring(4, 6), amount)}`;
 }
 
 /* Resta el porcentaje indicado a un color (RR, GG o BB) hexadecimal para oscurecerlo */
-export const subtractLight = function(color, amount){
-  let cc = parseInt(color,16) - amount;
+export const subtractLight = (color, amount) => {
+  const cc = parseInt(color, 16) - amount;
   let c = (cc < 0) ? 0 : (cc);
-  c = (c.toString(16).length > 1 ) ? c.toString(16) : `0${c.toString(16)}`;
+  c = (c.toString(16).length > 1) ? c.toString(16) : `0${c.toString(16)}`;
   return c;
 }
 
 /* Oscurece un color hexadecimal de 6 caracteres #RRGGBB segun el porcentaje indicado */
-export const darken = (color, amount) =>{
-  color = (color.indexOf("#")>=0) ? color.substring(1,color.length) : color;
-  amount = parseInt((255*amount)/100);
-  return color = `#${subtractLight(color.substring(0,2), amount)}${subtractLight(color.substring(2,4), amount)}${subtractLight(color.substring(4,6), amount)}`;
+export const darken = (colorOrigin, amountOrigin) => {
+  const color = (colorOrigin.indexOf('#') >= 0) ? colorOrigin.substring(1, colorOrigin.length) : colorOrigin;
+  const amount = parseInt((255 * amountOrigin) / 100, 10);
+  return `#${subtractLight(color.substring(0, 2), amount)}${subtractLight(color.substring(2, 4), amount)}${subtractLight(color.substring(4, 6), amount)}`;
 }
 
 export const hexToRGB = (hex, alpha) => {
-  const r = parseInt(hex.slice(1, 3), 16),
-      g = parseInt(hex.slice(3, 5), 16),
-      b = parseInt(hex.slice(5, 7), 16);
+  const r = parseInt(hex.slice(1, 3), 16)
+  const g = parseInt(hex.slice(3, 5), 16)
+  const b = parseInt(hex.slice(5, 7), 16)
 
-  const rgb = [r,g,b]
+  const rgb = [r, g, b]
 
   if (alpha) {
     return `rgba(${rgb.join(',')},${alpha})`;
-  } else {
-    return `rgb(${rgb.join(',')})`;
   }
+  return `rgb(${rgb.join(',')})`;
 }
