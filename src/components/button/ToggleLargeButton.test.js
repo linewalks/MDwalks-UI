81e7072bd89168b7c6b6044b6e955b717405227d
lyohan <yohan@linewalks.com>
2020-02-03 12:33:12 +0900

    feat: ToggleLargeButton Component와 test 추가
    1. data길이는 상관없고, [{type, text}] 형태에 데이터를 받는다
    2. selected된 항목은 click하여도 onChagne가 발생하지않는다
    3. ToggleLargeButton test를 위한 babel 설정 추가
    4. ToggleLargeButton test 추가
    
    close:#363

---
+++ src/components/button/ToggleLargeButton.test.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import _ from 'lodash'
+import ToggleLargeButton from '@Components/button/ToggleLargeButton'
+
+const data = [
+  {
+    type: 'AA',
+    text: '환자군',
+  },
+  {
+    type: 'BB',
+    text: '진단수'
+  },
+  {
+    type: 'CC',
+    text: '결과'
+  },
+]
+
+describe('ToggleLargeButton Component', () => {
+  let component;
+  let onChange;
+  beforeEach(() => {
+    onChange = jest.fn((value) => value);
+    component = mount(<ToggleLargeButton data={data} onChange={onChange} />)
+  })
+
+  it('첫번째 항목은 랜더링 후 selected 되어 있다', () => {
+    expect(component.find('button').at(0).prop('selected')).toEqual(true)
+  })
+
+  it('세번째 항목을 클릭하면 onChange 함수가 발생한다.', () => {
+    component.find('button').at(2).simulate('click')
+    expect(onChange).toHaveBeenCalled()
+  })
+
+  it('selected 된 항목은 click 하여도 onChange 가 발생하지 않는다', () => {
+    component.find('button').at(1).simulate('click')
+    expect(component.find('button').at(1).prop('selected')).toEqual(true)
+
+    component.find('button').at(1).simulate('click')
+    expect(onChange).toHaveBeenCalledTimes(1)
+  })
+
+
+  it('두번째 항목을 클릭하면 onChange 에 "BB" 을 넘긴다', () => {
+    component.find('button').at(1).simulate('click')
+    expect(onChange).toHaveBeenCalledWith('BB')
+  })
+
+  it('첫째 항목을 클릭하면 onChange 에 "AA" 을 넘긴다', () => {
+    component.find('button').at(2).simulate('click')
+
+
+    component.find('button').at(0).simulate('click')
+    expect(onChange).toHaveBeenCalledWith('AA')
+  })
+})
