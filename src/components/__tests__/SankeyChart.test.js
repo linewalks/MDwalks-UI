14855fcc71572e336c59e19c5a543ed635ef4f9d
jik0090 <jik0090@naver.com>
2020-02-27 16:50:23 +0900

    fix: Chart 자잘한 bug 수정
    1. LineMergedTimeline timeData propTypes 수정
    2. Sankey defaultnNode를 defaultNode로 오타수정
    
    close:#410

--- src/components/__tests__/SankeyChart.test.js
+++ src/components/__tests__/SankeyChart.test.js
@@ -179,7 +179,7 @@ describe('데이터를 가진 Sankey Chart가 데이터 변경시, 변경된 데
     wrapper.setProps({
       data: changedSankeyChartData,
       selectedNodes: ["A", "B"],
-      defaultdNode: ["A"]
+      defaultNode: ["A"]
     })
 
     expect(resetSpy).toHaveBeenCalled()
