597bdc20b96aeb86665cffad0bc00c2ec980e9a4
toyjhlee <toyjhlee@gmail.com>
2020-03-25 13:51:54 +0900

    feat(BarChart): add scroll option
    closes: #487

--- src/components/charts/BarChart.js
+++ src/components/charts/BarChart.js
@@ -2,6 +2,8 @@ import React from 'react'
 import _ from 'lodash'
 import * as Rechart from 'recharts'
 import PropTypes from 'prop-types'
+import { Scrollbars } from 'react-custom-scrollbars';
+
 import Heading from '@Components/layout/Heading'
 import EmptyPlaceHolder from '@Components/table/EmptyPlaceHolder'
 import { color } from '@src/assets/styles/variables'
@@ -31,6 +33,7 @@ const BarChart = ({
   yDataKey,
   theme,
   isPercent,
+  scroll,
 }) => {
   const colors = colorSet[theme] || colorSet.blue
   const newYDataKey = [].concat(yDataKey)
@@ -51,6 +54,8 @@ const BarChart = ({
   const XAxisTicFormatter = layout === 'horizontal' ? undefined : tickFormatter
   const YAxisTicFormatter = layout === 'horizontal' ? tickFormatter : undefined
 
+  const isScroll = !_.isUndefined(scroll.y)
+
   return (
     <div>
       <Heading size="18" style={{ marginBottom: '30px' }}>{title}</Heading>
@@ -60,20 +65,44 @@ const BarChart = ({
         isEmpty(data)
           ? <EmptyPlaceHolder />
           : (
-            <Rechart.ResponsiveContainer height={415}>
-              <Rechart.BarChart data={data} height={415} layout={layout}>
-                <Rechart.CartesianGrid vertical={false} stroke={color.$line_graph_xy_grey} />
-                <Rechart.XAxis tickLine={false} tickMargin={10} stroke="rgba(0, 0, 0, 0.6)" dataKey={XAxisDataKey} tickFormatter={XAxisTicFormatter} type={XAxisType} />
-                <Rechart.YAxis axisLine={false} tickMargin={10} stroke="rgba(0, 0, 0, 0.4)" tickLine={false} dataKey={YAxisDataKey} tickFormatter={YAxisTicFormatter} type={YAxisType} />
-                <Rechart.Tooltip
-                  isPercent={isPercent}
-                  content={TooltipBox}
-                />
-                {
-                  newYDataKey.map((entry, index) => (<Rechart.Bar key={`bar${entry}`} dataKey={entry} fill={colors[index]} stackId={stackId} />))
-                }
-              </Rechart.BarChart>
-            </Rechart.ResponsiveContainer>
+            <div>
+              <Scrollbars style={{ height: isScroll ? scroll.y : 415 }}>
+                <Rechart.ResponsiveContainer height={415}>
+                  <Rechart.BarChart
+                    data={data}
+                    height={415}
+                    layout={layout}
+                    margin={{
+                      top: 5, right: 5, bottom: (isScroll ? 0 : 5), left: 5,
+                    }}
+                  >
+                    <Rechart.CartesianGrid vertical={false} stroke={color.$line_graph_xy_grey} />
+                    <Rechart.XAxis hide={isScroll} tickLine={false} tickMargin={10} stroke="rgba(0, 0, 0, 0.6)" dataKey={XAxisDataKey} tickFormatter={XAxisTicFormatter} type={XAxisType} />
+                    <Rechart.YAxis axisLine={false} tickMargin={10} stroke="rgba(0, 0, 0, 0.4)" tickLine={false} dataKey={YAxisDataKey} tickFormatter={YAxisTicFormatter} type={YAxisType} />
+                    <Rechart.Tooltip
+                      isPercent={isPercent}
+                      content={TooltipBox}
+                    />
+                    {
+                      newYDataKey.map((entry, index) => (<Rechart.Bar key={`bar${entry}`} dataKey={entry} fill={colors[index]} stackId={stackId} />))
+                    }
+                  </Rechart.BarChart>
+                </Rechart.ResponsiveContainer>
+              </Scrollbars>
+              {
+                isScroll && (
+                  <Rechart.ResponsiveContainer height={36}>
+                    <Rechart.BarChart data={data} height={36} layout={layout}>
+                      <Rechart.XAxis tickLine={false} tickMargin={10} stroke="rgba(0, 0, 0, 0.6)" dataKey={XAxisDataKey} tickFormatter={XAxisTicFormatter} type={XAxisType} />
+                      <Rechart.YAxis axisLine={false} tickMargin={10} stroke="rgba(0, 0, 0, 0.4)" tickLine={false} dataKey={YAxisDataKey} tickFormatter={YAxisTicFormatter} type={YAxisType} />
+                      {
+                        newYDataKey.map((entry, index) => (<Rechart.Bar style={{ visibility: 'hidden' }} key={`bar${entry}`} dataKey={entry} fill={colors[index]} stackId={stackId} />))
+                      }
+                    </Rechart.BarChart>
+                  </Rechart.ResponsiveContainer>
+                )
+              }
+            </div>
           )
       }
     </div>
@@ -89,6 +118,7 @@ BarChart.defaultProps = {
   yDataKey: ['value', []],
   theme: 'blue',
   isPercent: false,
+  scroll: {},
 }
 
 BarChart.propTypes = {
@@ -100,6 +130,9 @@ BarChart.propTypes = {
   yDataKey: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
   theme: PropTypes.oneOf(['blue', 'green', 'compare']),
   isPercent: PropTypes.bool,
+  scroll: PropTypes.shape({
+    y: PropTypes.number,
+  }),
 }
 
 export default BarChart
