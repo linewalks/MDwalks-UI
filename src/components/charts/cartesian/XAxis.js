22c1925d391e3a8978a836bb14c84e7734b19abc
toyjhlee <toyjhlee@gmail.com>
2020-05-06 10:27:25 +0900

    feat(cartesian): create XAxis, YAxis
    closes: #564

---
+++ src/components/charts/cartesian/XAxis.js
@@ -0,0 +1,15 @@
+import * as Rechart from 'recharts'
+import { colorV1 } from '@src/assets/styles/variables'
+
+class XAxis extends Rechart.XAxis {
+}
+
+XAxis.defaultProps = {
+  ...Rechart.XAxis.defaultProps,
+  axisLine: { stroke: colorV1.$grey06 },
+  tickLine: false,
+  tickMargin: 10,
+  stroke: colorV1.$grey08,
+}
+
+export default XAxis
