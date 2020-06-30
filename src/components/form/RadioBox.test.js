6b582e78815892ace4f9247e544702a69507459c
thiporia <ssoju2827@gmail.com>
2020-06-30 15:22:01 +0900

    test: componentDidUpdate 테스트 추가

--- src/components/form/RadioBox.test.js
+++ src/components/form/RadioBox.test.js
@@ -31,4 +31,34 @@ describe('RadioBox', () => {
     wrapper.find('div label span input').at(1).simulate('change', { target: { value: 2 } })
     expect(callback).toBeCalledTimes(1)
   })
+
+  it('ComponentDidUpdate', () => {
+    const wrapper = mount(
+      <RadioBox
+        onChange={callback}
+        align="left"
+        data={
+          [
+            { id: 1, name: 'name 1', checked: true },
+            { id: 2, name: 'name 2' },
+            { id: 3, name: 'name 3' },
+            { id: 4, name: 'name 21' },
+            { id: 5, name: 'name 31' },
+            { id: 6, name: 'name 22' },
+            { id: 7, name: 'name 32' },
+            { id: 8, name: 'name 23' },
+            { id: 9, name: 'name 34' },
+          ]
+        }
+      />,
+    )
+
+    const newData = [
+      { id: 1, name: 'name 1' },
+      { id: 2, name: 'name 2', checked: true },
+    ]
+    wrapper.instance().setSelectedList = jest.fn()
+    wrapper.setProps({ data: newData })
+    expect(wrapper.instance().setSelectedList).toBeCalledTimes(1)
+  })
 })
