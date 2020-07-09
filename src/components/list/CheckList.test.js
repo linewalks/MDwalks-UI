967c6c6567266131fd5b512f772772a84a83530b
thiporia <ssoju2827@gmail.com>
2020-07-09 15:16:01 +0900

    feat(CheckList): hooks로 컴포넌트를 전환

--- src/components/list/CheckList.test.js
+++ src/components/list/CheckList.test.js
@@ -1,40 +1,12 @@
-import React from 'react';
-import { mount } from 'enzyme';
+import React from 'react'
+import { mount } from 'enzyme'
 import CheckList from '@Components/list/CheckList'
 import Item from '@Components/list/Item'
 import _ from 'lodash'
 
 import ChartConfig from '@src/helper/ChartConfig'
 
-const data = [
-  {
-    id: 1,
-    name: 'name 1',
-    checked: true,
-  },
-  {
-    id: 2,
-    name: 'name 2',
-  },
-  {
-    id: 3,
-    name: 'name 3',
-  },
-  {
-    id: 4,
-    name: 'name 4',
-  },
-  {
-    id: 5,
-    name: 'name 5',
-  },
-  {
-    id: 6,
-    name: 'name 6',
-  },
-]
-
-/**
+/*
  * 선택이 변경 시 onChange 발생, param 으로는 선택된 id 를 넘긴다
  * isDisabeld 시 선택이 되지 않는다
  * 6개 이상 선택 되지 않는다
@@ -44,74 +16,115 @@ const data = [
  * option: limit
  * props.checkVisible 가 false 인 경우 선택 된 것은 보이지 않는다
  */
-const limit = 5
-let wrapper
-let instance
-let onChange
-let onError
-let selectedList
+describe('defaut', () => {
+  const data = [
+    {
+      id: 1,
+      name: 'name 1',
+      checked: true,
+    },
+    {
+      id: 2,
+      name: 'name 2',
+    },
+    {
+      id: 3,
+      name: 'name 3',
+    },
+    {
+      id: 4,
+      name: 'name 4',
+    },
+    {
+      id: 5,
+      name: 'name 5',
+    },
+    {
+      id: 6,
+      name: 'name 6',
+    },
+  ]
 
-beforeEach(() => {
-  selectedList = ['1']
-  onChange = jest.fn()
-  onError = jest.fn()
-  wrapper = mount(<CheckList data={data} limit={limit} onChange={onChange} onError={onError} />)
-  instance = wrapper.instance()
-})
+  const limit = 5
+  let wrapper
+  let onChange
+  let onError
+  let selectedList
 
-it('default', () => {
-  const expected = data.map(({ name }) => name).join('')
-  expect(wrapper.state('selectedList')).toEqual(selectedList)
-  expect(wrapper.text()).toBe(expected)
-  expect(wrapper.prop('limit')).toBe(limit)
-  expect(wrapper.prop('checkVisible')).toBe(true)
-  expect(wrapper.prop('layout')).toBe(ChartConfig.Layout.VERTICAL)
-  expect(wrapper.find(Item).at(0).prop('layout')).toBe(ChartConfig.Layout.VERTICAL)
-})
+  beforeEach(() => {
+    selectedList = ['1']
+    onChange = jest.fn()
+    onError = jest.fn()
+    wrapper = mount(<CheckList data={data} limit={limit} onChange={onChange} onError={onError} />)
+  })
 
-it('check', () => {
-  instance.onChangeTrigger = jest.fn()
+  it('default', () => {
+    const expected = data.map(({ name }) => name).join('')
+    expect(wrapper.text()).toBe(expected)
+    expect(wrapper.prop('limit')).toBe(limit)
+    expect(wrapper.prop('checkVisible')).toBe(true)
+    expect(wrapper.prop('layout')).toBe(ChartConfig.Layout.VERTICAL)
+    expect(wrapper.find(Item).at(0).prop('layout')).toBe(ChartConfig.Layout.VERTICAL)
+  })
 
-  wrapper.find('input[type="checkbox"]').last().simulate('change')
+  it('check', () => {
+    expect(wrapper.find('input[type="checkbox"]').last().prop('checked')).toBeFalsy()
+    wrapper.find('input[type="checkbox"]').last().simulate('change')
+    wrapper.update()
+    expect(wrapper.find('input[type="checkbox"]').last().prop('checked')).toBeTruthy()
+  })
 
-  expect(instance.onChangeTrigger).toHaveBeenCalledWith(_.last(data).id)
-})
+  it('onChange', () => {
+    selectedList.push('6')
+    wrapper.find('input[type="checkbox"]').last().simulate('change')
+    expect(onChange).toHaveBeenCalledWith({ newSelectedList: selectedList })
 
-it('onChange', () => {
-  selectedList.push('6')
-  wrapper.find('input[type="checkbox"]').last().simulate('change')
-  expect(onChange).toHaveBeenCalledWith({ selectedList })
-  expect(instance.getCheckCount()).toBe(2)
-
-  selectedList = _.without(selectedList, '6')
-  wrapper.find('input[type="checkbox"]').last().simulate('change')
-  expect(onChange).toHaveBeenCalledWith({ selectedList })
-  expect(instance.getCheckCount()).toBe(1)
+    selectedList = _.without(selectedList, '6')
+    wrapper.find('input[type="checkbox"]').last().simulate('change')
+    expect(onChange).toHaveBeenCalledWith({ newSelectedList: selectedList })
+  })
 })
 
 describe('OnError', () => {
-  it('false 인 항목을 click 시 onError 호출, onChange 는 호출 되지 않는다, onError 호출 시 limit 를 넘긴다', () => {
-    wrapper.setState({
-      checkState: {
-        1: true, 2: true, 3: true, 4: true, 5: true, 6: false,
-      },
-    });
-    wrapper.setState({ selectedList: ['1', '2', '3', '4', '5'] });
+  const data = [
+    {
+      id: 1,
+      name: 'name 1',
+      checked: true,
+    },
+    {
+      id: 2,
+      name: 'name 2',
+      checked: true,
+    },
+    {
+      id: 3,
+      name: 'name 3',
+    },
+  ]
 
+  const limit = 2
+  let wrapper
+  let onChange
+  let onError
+
+  beforeEach(() => {
+    onChange = jest.fn()
+    onError = jest.fn()
+    wrapper = mount(<CheckList data={data} limit={limit} onChange={onChange} onError={onError} />)
+  })
+
+  it('false 인 항목을 click 시 onError 호출, onChange 는 호출 되지 않는다, onError 호출 시 limit 를 넘긴다', () => {
     wrapper.find('input[type="checkbox"]').last().simulate('change')
+    wrapper.update()
     expect(onError).toHaveBeenCalled()
     expect(onChange).not.toHaveBeenCalled()
     expect(onError).toHaveBeenCalledWith({ limit })
   })
 
   it('true 인 항목을 click 시 onError 호출 되지 않고, onChange 는 호출 된다', () => {
-    wrapper.setState({
-      checkState: {
-        1: true, 2: true, 3: true, 4: true, 5: true, 6: false,
-      },
-    });
-    wrapper.setState({ selectedList: ['1', '2', '3', '4', '5'] });
-
+    onError.mockClear()
+    onChange.mockClear()
     wrapper.find('input[type="checkbox"]').first().simulate('change')
     expect(onError).not.toHaveBeenCalled()
     expect(onChange).toHaveBeenCalled()
@@ -119,90 +132,83 @@ describe('OnError', () => {
 })
 
 describe('disabled', () => {
-  it('default', () => {
-    expect(wrapper.prop('disabled')).toBe(false)
-  })
+  const data = [
+    {
+      id: 1,
+      name: 'name 1',
+      checked: true,
+    },
+    {
+      id: 2,
+      name: 'name 2',
+      checked: true,
+    },
+    {
+      id: 3,
+      name: 'name 3',
+    },
+  ]
 
-  it('set', () => {
-    wrapper.setProps({ disabled: true })
-    expect(wrapper.prop('disabled')).toBe(true)
+  const limit = 2
+  let wrapper
+  let onChange
+  let onError
+
+  beforeEach(() => {
+    onChange = jest.fn()
+    onError = jest.fn()
+    wrapper = mount(
+      <CheckList data={data} limit={limit} disabled onChange={onChange} onError={onError} />,
+    )
   })
 
   it('disabled 인 경우 click 되지 않는다', () => {
-    wrapper.setProps({ disabled: true })
-    instance.onChangeTrigger = jest.fn()
-
     expect(wrapper.find('input[type="checkbox"]').first().prop('disabled')).toBe(true)
-
     wrapper.find('input[type="checkbox"]').first().simulate('change')
-    expect(instance.onChangeTrigger).toHaveBeenCalled()
     expect(onError).not.toHaveBeenCalled()
     expect(onChange).not.toHaveBeenCalled()
   })
 })
 
-describe('unCheckedById', () => {
-  it('해당 ID 가 true 인 경우 onChange 호출', () => {
-    instance.unCheckedById(1)
-
-    selectedList = _.without(selectedList, '1')
-    expect(onChange).toHaveBeenCalledWith({ selectedList })
-  })
-
-  it('해당 ID 가 false 인 경우 onChange 호출 되지 않는다', () => {
-    instance.unCheckedById(2)
-
-    selectedList = _.without(selectedList, '2')
-    expect(onChange).not.toHaveBeenCalled()
-  })
-})
-
 describe('checkVisible 가 false 인 경우', () => {
+  const data = [
+    {
+      id: 1,
+      name: 'name 1',
+      checked: true,
+    },
+    {
+      id: 2,
+      name: 'name 2',
+      checked: true,
+    },
+    {
+      id: 3,
+      name: 'name 3',
+    },
+  ]
+
+  const limit = 2
+  let wrapper
+  let onChange
+  let onError
+
+  beforeEach(() => {
+    onChange = jest.fn()
+    onError = jest.fn()
+    wrapper = mount(
+      <CheckList
+        data={data}
+        limit={limit}
+        checkVisible={false}
+        onChange={onChange}
+        onError={onError}
+      />,
+    )
+  })
+
   it('default', () => {
-    wrapper.setProps({ checkVisible: false })
     const expected = data.map(({ name, checked }) => (checked ? '' : name)).join('')
     expect(wrapper.text()).toBe(expected)
-  })
-})
-
-describe('props.data에 변환에 따른 state.selectedList 변화 확인', () => {
-  it('default', () => {
-    const newData = [
-      {
-        id: 1,
-        name: 'name 1',
-      },
-      {
-        id: 2,
-        name: 'name 2',
-        checked: true,
-      },
-      {
-        id: 3,
-        name: 'name 3',
-        checked: true,
-      },
-      {
-        id: 4,
-        name: 'name 4',
-      },
-      {
-        id: 5,
-        name: 'name 5',
-        checked: true,
-      },
-      {
-        id: 6,
-        name: 'name 6',
-      },
-    ]
-    const expected = ['1']
-    expect(wrapper.state('selectedList')).toEqual(expected)
-
-    wrapper.setProps({ data: newData })
-    wrapper.update()
-
-    const newExpected = ['2', '3', '5']
-    expect(wrapper.state('selectedList')).toEqual(newExpected)
   })
 })
