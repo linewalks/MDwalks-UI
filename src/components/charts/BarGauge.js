510fe3dd545eafb86a292b21e694d8cf4d664ffc
LeeYoHan <jik0090@naver.com>
2020-09-07 16:47:11 +0900

    feat: BarGauge 임계치 선 추가
    1. threshold props 값을 받으면 임계치 추가
    2. threshold 테스트 코드 추가
    
    closes: #739

--- src/components/charts/BarGauge.js
+++ src/components/charts/BarGauge.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import _ from 'lodash'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+import { color, colorV1 } from '@src/assets/styles/variables'
 
 import {
   getColorsByTheme,
@@ -9,26 +10,52 @@ import {
 } from '@Components/ChartColor'
 
 const Container = styled.div`
-  background: #f2f2f2;
   position: relative;
-  height: 6px;
+`
+
+const FillContainer = styled.div`
+  background: #f2f2f2;
+  height: 12px;
   width: 100px;
   border-radius: 2px;
   overflow: hidden;
+`
+
+const fillLinear = keyframes`
+  0% {
+    width: 0;
+  }
 `
 
 const Fill = styled.div`
   background-image: ${(props) => (props.theme ? `linear-gradient(to right, ${props.theme[0]}, ${props.theme[2]} 100px)` : 'auto')};
   height: 100%;
   width: ${(props) => (_.isNumber(props.score) ? `${props.score}%` : 'auto')};
+  animation: ${fillLinear} 1.5;
 `
 
-const BarGauge = ({ score, theme }) => {
+const Threshold = styled.div`
+  position: absolute;
+  width: 3px;
+  height: 16px;
+  top: 0;
+  left: ${(props) => (_.isNumber(props.threshold) && `${props.threshold}px`)};
+  margin-top: -2px;
+  margin-left: -1.5px;
+  background-color: ${colorV1.$red01};
+  border: 1px solid ${color.$primary_white};
+  border-radius: 0.5px;
+`
+
+const BarGauge = ({ score, theme, threshold }) => {
   const colors = getColorsByTheme(theme)
   if (_.inRange(score, 0, 101)) {
     return (
       <Container>
-        <Fill score={score} theme={colors} />
+        {threshold && _.inRange(threshold, 0, 101) && <Threshold threshold={threshold} />}
+        <FillContainer>
+          <Fill score={score} theme={colors} />
+        </FillContainer>
       </Container>
     )
   }
@@ -40,10 +67,12 @@ const BarGauge = ({ score, theme }) => {
 
 BarGauge.defaultProps = {
   theme: Themes.ThemeArrangeGradientPrimarySea,
+  threshold: undefined,
 }
 
 BarGauge.propTypes = {
   score: PropTypes.number.isRequired,
+  threshold: PropTypes.number,
   theme: PropTypes.oneOf([
     Themes.ThemeArrangeGradientPrimarySea,
     Themes.ThemeArrangeGradientSecondaryTeal,
