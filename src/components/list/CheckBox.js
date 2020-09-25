8413abac045e733d45aa6a51347a8a1af4b58b4d
jik0090 <jik0090@naver.com>
2020-09-25 16:37:19 +0900

    fix(CheckBox): useEffect 추가
    1. defaultChecked 변경 시, 반영 되도록 useEffect 추가
    2. defaultChecked 테스트 추가
    
    closes: #758

--- src/components/list/CheckBox.js
+++ src/components/list/CheckBox.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 import Item from '@Components/list/Item'
@@ -17,6 +17,10 @@ const CheckBox = ({
     setChecked(evt.target.checked)
     onChange(evt.target.checked)
   }
+
+  useEffect(() => {
+    setChecked(defaultChecked)
+  }, [defaultChecked])
 
   return (
     <Item disabled={disabled}>
