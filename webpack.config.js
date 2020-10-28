63d8ef402ed431a15e33806992a7accb6f364db6
jik0090 <jik0090@naver.com>
2020-10-28 15:06:46 +0900

    feat(Table): defaultSort 추가
    1. defaultSort 추가
    2. 소트순서 컨트롤 하도록 sortOrderList 추가
    
    closes: #770

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,6 @@ module.exports = {
         use: [
           {
             loader: 'style-loader',
-            options: { sourceMap: true },
           },
           {
             loader: 'css-loader',
