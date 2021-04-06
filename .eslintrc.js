8e051838de3532cc6abe365ded1343f636a7e73a
lahee <lahee@linewalks.com>
2021-04-06 15:54:26 +0900

    chore: apply new eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,55 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "env": {
-        "browser": true,
-        "es6": true,
-        "jest": true,
-        "commonjs": true
+  // 스크립트가 실행되도록 설계된 환경. 각 환경은 미리 정의 된 특정 전역 변수 집합을 제공합니다.
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    commonjs: true,
+  },
+  // 추가 규칙
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "airbnb",
-        "airbnb/hooks"
-    ] ,
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
     ],
-    "rules": {
-        'react/jsx-filename-extension': [1, {
-          "extensions": [".js", ".jsx", ".ts", "tsx"]
-        }],
-        // 더블도 지원해야 하는 경우가 있을까?
-        'quotes': ['error', 'single', {
-          'allowTemplateLiterals': true
-        }],
-        'semi': ['off', 'always'],
-        // package.json 의 alias 를 가져 와야 하는가?
-        "import/no-unresolved": "off",
-        // children 를 선언할 필요는 없지 않을까?
-        "react/prop-types": [2, { ignore: ['children', 'style'] }],
-        "no-console": ["error", { allow: ["warn", "error"] }],
-    },
-};
+    quotes: [
+      // 더블도 지원해야 하는 경우가 있을까?
+      'error',
+      'single',
+      {
+        allowTemplateLiterals: true,
+      },
+    ],
+    semi: ['off', 'always'],
+    'import/no-unresolved': 'off', // package.json 의 alias 를 가져 와야 하는가?
+    'react/prop-types': [2, { ignore: ['children', 'style'] }], // children 를 선언할 필요는 없지 않을까?
+    'implicit-arrow-linebreak': 0,
+    'operator-linebreak': 0, // 해당 룰로 인해서 라인이 부득이하게 길어지는 경우가 발생하여 off
+    'react/forbid-prop-types': [0], // prop-types에서 [any, object, array]를 사용할 경우가 있음.
+    'no-param-reassign': [2, { props: false }], // SSR 시에 res에 reassign을 해야함.
+    'object-curly-newline': [
+      2, // 해당 기능은 prettier와 충돌이 있어 제외합니다.
+      {
+        ObjectExpression: 'never',
+        multiline: true, // import 과정에서 multiline 패턴이 있으므로 true로 설정.
+        ImportDeclaration: 'never',
+      },
+    ],
+  },
+}
